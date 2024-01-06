2e99e20f1f978ec01ddbf4b0ddccda82fe4a0948
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-06 18:15:54 +0100

    Animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,98 @@ export default {
       fontFamily: {
         cyber: ['Cyber', 'sans-serif'],
       },
+      animation: {
+        'glitch-1': 'glitch-1 5s linear infinite',
+        'glitch-2': 'cyber-glitch-1 5s linear infinite',
+      },
+      keyframes: {
+        'glitch-1': {
+          '0%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '2%': {
+            filter: 'blur(0.05rem) brightness(120%) contrast(120%)',
+          },
+          '4%': {
+            filter: 'blur(0.1rem) brightness(150%) contrast(150%)',
+          },
+          '6%': {
+            filter: 'blur(0.2rem) brightness(170%) contrast(170%)',
+          },
+          '8%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '10%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '60%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '62%': {
+            filter: ' blur(0.05rem) brightness(60%) contrast(100%)',
+          },
+          '64%': {
+            filter: 'blur(0.1rem) brightness(40%) contrast(100%)',
+          },
+          '66%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '68%': {
+            filter: 'blur(0.05rem) brightness(60%) contrast(100%)',
+          },
+          '70%': {
+            filter: 'blur(0.1rem) brightness(40%) contrast(100%)',
+          },
+          '72%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+          '100%': {
+            filter: 'blur(0) brightness(100%) contrast(100%)',
+          },
+        },
+        'glitch-2': {
+          '0%': {
+            transform: 'translate(0, 0) skew(0deg)',
+            filter: 'invert(0)',
+          },
+          '1%': {
+            transform: 'translate(0, 0) skew(-3deg)',
+            filter: 'invert(0)',
+          },
+          '2%': {
+            transform: 'translate(0, 0) skew(3deg)',
+            filter: 'invert(0)',
+          },
+          '3%': {
+            transform: 'translate(0, 0) skew(0deg)',
+            filter: 'invert(0)',
+          },
+          '60%': {
+            transform: ',translate(0, 0) skew(0deg)',
+            filter: 'invert(0)',
+          },
+          '61%': {
+            transform: 'translate(0, 0) skew(-3deg)',
+            filter: 'invert(0.2)',
+          },
+          '62%': {
+            transform: ' translate(0, 0) skew(0deg)',
+            filter: 'invert(0.2)',
+          },
+          '63%': {
+            transform: 'translate(0, 0) skew(3deg)',
+            filter: 'invert(0.8)',
+          },
+          ' 64%': {
+            transform: 'translate(0, 0) skew(0deg)',
+            filter: 'invert(0.2)',
+          },
+          '65%': {
+            transform: 'translate(0, 0) skew(0deg)',
+            filter: 'invert(0)',
+          },
+        },
+      },
     },
   },
   // eslint-disable-next-line global-require
