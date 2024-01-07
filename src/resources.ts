88c6a71161cf1fcf747d46e27fbfd1e1b1d36731
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-07 18:31:15 +0100

    Public Assets

--- src/resources.ts
+++ src/resources.ts
@@ -3,25 +3,25 @@ const resources = [
     id: 1,
     name: 'Overachiever',
     details: 'This project is Overachiever',
-    imageUrl: '../src/assets/project-1.png',
+    imageUrl: '../public/assets/project-1.png',
   },
   {
     id: 2,
     name: 'Github Finder',
     details: 'This project is Github Finder',
-    imageUrl: '../src/assets/project-2.png',
+    imageUrl: '../public/assets/project-2.png',
   },
   {
     id: 3,
     name: 'Shortly',
     details: 'This is Shortly',
-    imageUrl: '../src/assets/project-3.png',
+    imageUrl: '../public/assets/project-3.png',
   },
   {
     id: 4,
     name: 'Fylo',
     details: 'This is Fylo',
-    imageUrl: '../src/assets/project-4.png',
+    imageUrl: '../public/assets/project-4.png',
   },
 ];
 
