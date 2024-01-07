f60ebe6a63c2b0ac1e9b4ee518de863ab98382a8
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-07 18:19:37 +0100

    Remove Template File

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,30 +1,30 @@
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import resources from './resources';
+// import { useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
+// import resources from './resources';
 
-function Template() {
-  const [projects, setProjects] = useState(resources);
-  const navigate = useNavigate();
+// function Template() {
+//   const [projects, setProjects] = useState(resources);
+//   const navigate = useNavigate();
 
-  return (
-    <div className="min-h-screen bg-accent ">
-      <h1 className="text-primary-content">Projects</h1>
-      <div className="text-primary-content">
-        {projects.map((project) => (
-          <button
-            className="btn btn-outline"
-            type="button"
-            key={project.id}
-            onClick={() => {
-              navigate(`/${project.id}`);
-            }}
-          >
-            {project.name}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="min-h-screen bg-accent ">
+//       <h1 className="text-primary-content">Projects</h1>
+//       <div className="text-primary-content">
+//         {projects.map((project) => (
+//           <button
+//             className="btn btn-outline"
+//             type="button"
+//             key={project.id}
+//             onClick={() => {
+//               navigate(`/${project.id}`);
+//             }}
+//           >
+//             {project.name}
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Template;
+// export default Template;
