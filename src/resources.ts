50e23d4ddeed904cae7e202a071e53124ecb9312
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-07 18:34:28 +0100

    Pathing to public assets

--- src/resources.ts
+++ src/resources.ts
@@ -3,25 +3,25 @@ const resources = [
     id: 1,
     name: 'Overachiever',
     details: 'This project is Overachiever',
-    imageUrl: '../public/assets/project-1.png',
+    imageUrl: '/assets/project-1.png',
   },
   {
     id: 2,
     name: 'Github Finder',
     details: 'This project is Github Finder',
-    imageUrl: '../public/assets/project-2.png',
+    imageUrl: '/assets/project-2.png',
   },
   {
     id: 3,
     name: 'Shortly',
     details: 'This is Shortly',
-    imageUrl: '../public/assets/project-3.png',
+    imageUrl: '/assets/project-3.png',
   },
   {
     id: 4,
     name: 'Fylo',
     details: 'This is Fylo',
-    imageUrl: '../public/assets/project-4.png',
+    imageUrl: '/assets/project-4.png',
   },
 ];
 
