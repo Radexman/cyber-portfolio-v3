1fff1717587a48c52dd8703b0e540a32c68e7224
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-10 15:59:06 +0100

    Render Tiles Conditionally

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,31 +1,28 @@
+import { useState } from 'react';
+import TileLabels from '../../TileLabels';
+import { LabelsTypes } from '../../Types/labels.types';
+
 function Stats() {
+  const [labels] = useState<LabelsTypes[]>(TileLabels);
+
+  const renderPairTiles = (tiles: LabelsTypes[]) => {
+    return tiles.map((tile) => (
+      <div key={tile.id} className="flex h-28 flex-col items-center justify-center space-y-2 bg-primary">
+        <p className="text-4xl text-secondary-content">{tile.value}</p>
+        <p>{tile.label}</p>
+      </div>
+    ));
+  };
+
   return (
-    <div className="container mx-auto py-4">
-      <div className="flex flex-col items-center justify-center space-y-7 p-2 md:flex-row md:space-x-8 md:space-y-0">
+    <div className="container mx-auto mt-4">
+      <div className="flex flex-col items-center justify-center space-y-7 md:flex-row md:space-x-8 md:space-y-0">
         <div className="flex w-[90%] flex-col items-center justify-center text-center md:w-1/3">
           <p className="text-8xl text-secondary-content">2+</p>
           <p className="text-2xl">Years Experience</p>
         </div>
-        <div className="flex w-[90%] flex-col space-y-8 md:w-1/3">
-          <div className="flex h-32 flex-col items-center justify-center space-y-2 bg-primary">
-            <p className="text-4xl text-secondary-content">2062+</p>
-            <p>Project Hours</p>
-          </div>
-          <div className="flex h-32 flex-col items-center justify-center space-y-2 bg-primary">
-            <p className="text-4xl text-secondary-content">3</p>
-            <p>Clients</p>
-          </div>
-        </div>
-        <div className="w-[90%] flex-col space-y-8 md:w-1/3">
-          <div className="flex h-32 flex-col items-center justify-center space-y-2 bg-primary">
-            <p className="text-4xl  text-secondary-content">2</p>
-            <p>Certificates</p>
-          </div>
-          <div className="flex h-32 flex-col items-center justify-center space-y-2 bg-primary">
-            <p className="text-4xl text-secondary-content">66+</p>
-            <p>Projects Deployed</p>
-          </div>
-        </div>
+        <div className="flex w-[90%] flex-col space-y-8 md:w-1/3">{renderPairTiles(labels.slice(0, 2))}</div>
+        <div className="w-[90%] flex-col space-y-8 md:w-1/3">{renderPairTiles(labels.slice(2, 4))}</div>
       </div>
     </div>
   );
