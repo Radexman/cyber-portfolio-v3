dd93231db1f4bb53cc78017a3dc6e38ecda24bdf
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-01-28 19:51:01 +0100

    Mobile Project Preview

--- src/pages/ProjectDisplay/ProjectDisplay.tsx
+++ src/pages/ProjectDisplay/ProjectDisplay.tsx
@@ -13,13 +13,13 @@ function ProjectDisplay() {
 
   return (
     <div className="container mx-auto">
-      <div className="min-h-[60vh] text-primary-content">
-        <div className="m-4 bg-primary p-4">
-          <div className="flex flex-col space-y-3 md:flex-row md:space-x-5 md:space-y-0">
-            <div className="w-full md:w-1/3">
+      <div className="min-h-[51vh] text-primary-content">
+        <div className="bg-secondary-focus mt-11 p-4 lg:bg-primary">
+          <div className="flex flex-col space-y-3 lg:flex-row lg:space-x-5 lg:space-y-0">
+            <div className="w-full lg:w-1/3">
               <img src={resourceImage} alt={`${resourceName} landing page`} />
             </div>
-            <div className="flex w-full flex-col justify-start space-y-2 md:w-2/3">
+            <div className="flex w-full flex-col justify-start space-y-2 lg:w-2/3">
               <h1 className="text-5xl">{resourceName}</h1>
               <div className="flex flex-wrap gap-1">
                 <div className="badge badge-warning badge-outline">Frontend</div>
@@ -29,8 +29,8 @@ function ProjectDisplay() {
                 <div className="badge badge-outline">Mobile First</div>
                 <div className="badge badge-success badge-outline">Accesibility</div>
               </div>
-              <p className="pt-2">{resourceSummary}</p>
-              <div className="flex items-end justify-end">
+              <p className="pt-2 text-justify lg:text-left">{resourceSummary}</p>
+              <div className="flex items-center justify-center lg:items-end lg:justify-end">
                 <button
                   type="button"
                   className="cyber-button-small bg-red fg-white z-30 -ml-5 mt-2 scale-75 lg:-ml-0 lg:scale-90"
