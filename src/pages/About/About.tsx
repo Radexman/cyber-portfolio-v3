2d10fe90b7ea8142b10e8042d13894888f2ade30
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-02-17 18:04:43 +0100

    Bug Fix

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -104,14 +104,16 @@ function About() {
           </div>
           <div className="flex flex-col items-center justify-between bg-secondary p-4 text-center shadow-xl transition hover:scale-105 md:items-start md:p-6 md:text-left">
             <h3 className="text-2xl">Experience</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Reprehenderit, eius.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Reprehenderit, eius.
-            </p>
+            <div className="pt-4">
+              <p className="text-lg">Commercial Work</p>
+              <div className="h-[2px] w-80 bg-primary" />
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. </p>
+            <div className="pt-4">
+              <p className="text-lg">Private Projects</p>
+              <div className="h-[2px] w-80 bg-primary" />
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. </p>
           </div>
           <div className="flex flex-col items-center justify-between space-y-2 bg-primary p-4 text-center shadow-xl transition hover:scale-105 md:items-start md:p-6 md:text-left">
             <h3 className="text-2xl text-secondary-content">Qualifications</h3>
