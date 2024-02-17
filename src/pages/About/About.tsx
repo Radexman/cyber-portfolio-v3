fbf02be53db9c482bed764b1b018ee7e5b57be30
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-02-17 16:07:30 +0100

    About Page Scaffold

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,22 @@
-import { Link } from 'react-router-dom';
+import profilePicture from '../../assets/profile-picture.png';
 
 function About() {
   return (
-    <div className="min-h-screen text-base-100">
-      <h1>About</h1>
-      <Link to="/">
-        <button type="button">Back To Home</button>
-      </Link>
+    <div className="min-h-screen">
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-center gap-2 text-primary-content md:flex-row">
+          <div className="w-full md:w-1/3">
+            <div className="avatar">
+              <div className="w-[50%] rounded">
+                <img src={profilePicture} alt="Front facing young man with brown hair" />
+              </div>
+            </div>
+          </div>
+          <div className="w-full bg-primary p-4 md:w-2/3">
+            <h1 className="text-2xl uppercase">Bio</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
