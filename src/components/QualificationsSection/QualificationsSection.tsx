5bb5aff1e8e35244385aa42b27ff72debe3f48be
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-02-18 18:18:48 +0100

    Qualifications Section Content

--- src/components/QualificationsSection/QualificationsSection.tsx
+++ src/components/QualificationsSection/QualificationsSection.tsx
@@ -1,15 +1,18 @@
+import { FaReact as ReactIcon } from 'react-icons/fa';
+
 function QualificationsSection() {
   return (
     <div className="flex flex-col items-center justify-between space-y-2 bg-primary p-4 text-center shadow-xl transition hover:scale-105 md:items-start md:p-6 md:text-left">
       <h3 className="text-2xl text-secondary-content">Qualifications</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Reprehenderit, eius.
+        I have acquiered multiple certifications from the frontend compenencies including React JavaScript and Tailwind
+        CSS.
       </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, eius. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Reprehenderit, eius.
-      </p>
+      <ul>
+        <li>React</li>
+        <li>JavaScrit</li>
+        <li>Tailwind CSS</li>
+      </ul>
     </div>
   );
 }
